9591cb2b3a03ae0694f2bd3434d165d5ebf8e110
goamlcz <121222736+goamlcz@users.noreply.github.com>
2023-01-04 10:31:57 +0100

    Create platno.py

---
+++ lekce 5/platno.py
@@ -0,0 +1,9 @@
+import tkinter # import knihovny pro grafické rozhraní
+# vytvoří prázdné okno
+canvas = tkinter.Canvas() 
+canvas.pack()
+
+# zde se vkládají funkce pro kreslení objektů
+
+# zajistí, aby okno zůstalo otevřené
+canvas.mainloop()
