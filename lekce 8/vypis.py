e6be2db332f1096d65c2649bceba42a29ea78243
goamlcz <121222736+goamlcz@users.noreply.github.com>
2023-01-11 15:10:08 +0100

    Create vypis.py

---
+++ lekce 8/vypis.py
@@ -0,0 +1,12 @@
+def vypis_text():
+    print('*****************')
+    print('** I am Python **')
+    print('*****************')
+
+print('Hello!')
+vypis_text()
+print('How are you')
+vypis_text()
+print('I am fine')
+vypis_text()
+print('This is end')
