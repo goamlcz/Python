c147714da8738fda17129e8267dcb4bc09e764b3
goamlcz <121222736+goamlcz@users.noreply.github.com>
2023-01-19 13:37:32 +0100

    Create obrazce.py

---
+++ lekce 8/obrazce.py
@@ -0,0 +1,29 @@
+def noha():
+    print('  |  ')
+    print('__|__')
+    
+def obdelnik():
+    print("#####")
+    print("#   #")
+    print("#####")
+   
+def trojuhelnik():
+    print("  *  ")
+    print(" *** ")
+    print("*****")
+
+trojuhelnik()
+trojuhelnik()
+noha()
+
+print()
+
+trojuhelnik()
+obdelnik()
+noha()
+
+print()
+
+obdelnik()
+noha()
+obdelnik()
