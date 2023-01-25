a21ceb677d83b9abd880b9647c0e9b1bc081b2cf
goamlcz <121222736+goamlcz@users.noreply.github.com>
2023-01-25 10:09:33 +0100

    Create kostka.py

---
+++ lekce 9/kostka.py
@@ -0,0 +1,16 @@
+import random
+
+def hod_kostkou():
+    n = random.randint(1,6)
+    print('Na kostce padla', n)
+
+hod_kostkou()
+hod_kostkou()
+hod_kostkou()
+hod_kostkou()
+hod_kostkou()
+hod_kostkou()
+hod_kostkou()
+hod_kostkou()
+hod_kostkou()
+hod_kostkou()
